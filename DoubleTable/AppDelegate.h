a22aa6da07a17cce8f132a61a70c07066696292f
HanOBa <565868472@qq.com>
2018-06-26 14:06:27 +0800

    Initial Commit

---
+++ DoubleTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubleTable
+//
+//  Created by HanOBa on 2018/6/26.
+//  Copyright © 2018年 HanOBa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
